fa413624bcd4ecb969f6e42f98121f12657d0c31
Bo Liang <rbaggio018@gmail.com>
2013-08-30 15:51:59 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pearson::Application.config.secret_token = 'd6837e6ea8d5a3e2b235898c7fb0ac7a8020ed3a8ba3b080eef2782093e8eb23618b99a3c6e8c28415806e50ad07ef5af46e88b42ec07f71264e51f9a6a18620'
