3329b362eb4fce740361b91c89a1b3f8ad68866a
Bo Liang <rbaggio018@gmail.com>
2013-09-02 17:32:09 -0400

    Associate book with existing publisher based on publisher code

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,17 @@ class Book < ActiveRecord::Base
   attr_accessible :book_code, :publisher_id, :title, :publisher_attributes, :chapters_attributes
 
   has_many :chapters, :dependent => :destroy
-  belongs_to :publisher
+  belongs_to :publisher, :autosave => true
 
   accepts_nested_attributes_for :publisher, :chapters
 
   validates :book_code, :uniqueness => true
+
+  def autosave_associated_records_for_publisher
+    if existing_publisher = Publisher.find_by_publisher_code(publisher.publisher_code)
+      self.publisher = existing_publisher
+    else
+      self.publisher = publisher if self.publisher.save
+    end
+  end
 end
